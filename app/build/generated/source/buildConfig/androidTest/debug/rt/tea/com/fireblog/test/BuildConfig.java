84bfd8398f7c0bdc58612b6369b675a585dd9e67
Anshu2502 <anshulkhare50@gmail.com>
2016-12-28 23:34:28 -0800

    FireBase Blog

---
+++ app/build/generated/source/buildConfig/androidTest/debug/rt/tea/com/fireblog/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package rt.tea.com.fireblog.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "rt.tea.com.fireblog.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
